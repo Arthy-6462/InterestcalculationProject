076c2648c324cdb4ae6446696bebb3958830c4c7
Arthy <arthy.apple@gmail.com>
2024-03-06 16:24:00 +0530

    simple interest commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Calculateinterest from './component/Calculateinterest';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Calculateinterest/>
+    </div>
+  );
+}
+
+export default App;
